82c943febe4bcb7cf6072125a2dd694114d3dd47
SouichiIshii <soichi.ishii.bc@ambl.co.jp>
2024-07-10 16:43:11 +0900

    tmp impl streamlit_app.py

---
+++ streamlit_app.py
@@ -0,0 +1,73 @@
+from datetime import datetime
+
+import streamlit as st
+import pandas as pd
+import requests
+
+
+API_URL = 'http://127.0.0.1:8000/tasks'
+
+
+def list_tasks():
+    response = requests.get(url=API_URL)
+    data = response.json()
+    print("API Response: ", data)
+    if not data:
+        return pd.DataFrame()
+    return pd.DataFrame(data)
+
+def create_task(task_data):
+    response = requests.post(
+        url=API_URL,
+        json=task_data
+    )
+    return response.json()
+
+def update_task(id, task_data):
+    response = requests.put(
+        url=f'{API_URL}/{id}',
+        json=task_data
+    )
+    return response.json()
+
+def delete_task(id):
+    response = requests.delete(url=f'{API_URL}/{id}')
+    return response.json()
+
+def task_form(task=None):
+    with st.form("task_form"):
+        task_name = st.text_input("タスク名", value='' if task is None else task["task_name"])
+        registration_date = datetime.today().strftime("%Y-%m-%d")
+        deadline_date = st.date_input("期限日", value=datetime.now() if task is None else datetime.strptime(task["deadline_date"], '%Y-%m-%d'))
+        status = st.selectbox("タスクの状況を入力してください。", options=["未着手", "作業中", "完了"], index=0 if task is None else ["未着手", "作業中", "完了"].index(task["status"]))
+
+        submit_button = st.form_submit_button("登録" if task is None else "更新")
+        if submit_button:
+            task_data = {
+                "task_name": task_name,
+                "registration_date": registration_date,
+                "deadline_date": deadline_date.strftime('%Y-%m-%d'),
+                "status": status
+            }
+            print("Sending Data:", task_data)
+            if task is None:
+                create_task(task_data=task_data)
+            else:
+                update_task(id=task["id"], task_data=task_data)
+
+
+if __name__ == "__main__":
+    tasks = list_tasks()
+    if not tasks.empty:
+        st.write("タスク一覧", tasks)
+    else:
+        st.write("タスク一覧", "現在登録されているタスクはありません。")
+
+    task_form()
+
+    for index, task in tasks.iterrows():
+        col1, col2, col3 = st.columns([1, 1, 8])
+        if col1.button("編集", key=f'edit-{index}'):
+            task_form(task=task)
+        if col2.button("削除", key=f'delete-{index}'):
+            delete_task(task["id"])
